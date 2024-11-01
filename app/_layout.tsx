3bcf16de36997ec185a9d364c24f74298fdc9ad9
程宗鑫 <>
2024-11-01 22:03:39 +0800

    clear

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen name="(tabs)" options={{ headerShown: false, title: 'Home' }} />
+    </Tabs>
   );
 }
